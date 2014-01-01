ef1e17a34ffda97e6f8f7d7ebc0499b707721d45
Anuradha Weeraman <aweeraman@gmail.com>
2014-01-01 12:23:47 +0530

    Halfway there

--- src/main/java/com/goalmeister/LoginActivity.java
+++ src/main/java/com/goalmeister/LoginActivity.java
@@ -4,10 +4,13 @@ import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
-import android.content.Intent;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -28,6 +31,9 @@ public class LoginActivity extends ActionBarActivity {
 
   ActionBar actionBar;
 
+  @SystemService
+  AccountManager accountManager;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -40,11 +46,24 @@ public class LoginActivity extends ActionBarActivity {
   }
 
   @UiThread
-  public void authenticationResult(boolean successful) {
+  public void authenticationResult(boolean successful, String username, String password,
+      String token) {
     if (successful) {
-      Intent intent = new Intent(this, DashboardActivity_.class);
-      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-      startActivity(intent);
+      Account account = new Account(username, "com.goalmeister");
+      boolean created = accountManager.addAccountExplicitly(account, null, null);
+      accountManager.setAuthToken(account, "com.goalmeister", token);
+      Bundle extras = getIntent().getExtras();
+      if (extras != null) {
+        if (created) {
+          AccountAuthenticatorResponse response =
+              extras.getParcelable(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
+          Bundle result = new Bundle();
+          result.putString(AccountManager.KEY_ACCOUNT_NAME, username);
+          result.putString(AccountManager.KEY_ACCOUNT_TYPE, "com.goalmeister");
+          response.onResult(result);
+        }
+      }
+      finish();
     } else {
       Toast.makeText(this, getResources().getString(R.string.failed_login), Toast.LENGTH_LONG)
           .show();
@@ -58,6 +77,7 @@ public class LoginActivity extends ActionBarActivity {
 
   @Background
   public void authenticateInBackground(String username, String password) {
-    authenticationResult(app.getServiceHelper().authenticate(username, password));
+    authenticationResult(app.getServiceHelper().authenticate(username, password), username,
+        password, app.getAccessToken());
   }
 }
